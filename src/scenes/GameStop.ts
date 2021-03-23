da3aae3ec8d84535a6bba5272f9d61272608df1d
jfx21 <81051927+jfx21@users.noreply.github.com>
2021-03-23 22:34:01 +0100

    Update GameStop.ts

--- src/scenes/GameStop.ts
+++ src/scenes/GameStop.ts
@@ -1,6 +1,6 @@
 
 import Phaser, { DOM} from 'phaser'
-import {primaryButton} from '../ui/button'
+import {primaryButton} from '../ui/button.jsx'
 
 export default class LevelFinishedScene extends Phaser.Scene 
 {
